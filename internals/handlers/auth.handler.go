a35a81c8a95059ebb21447fa569e19cbf5ad8530
metgag <moreehigh1@gmail.com>
2025-09-05 16:24:29 +0700

    feat: add auth handler to register and login

---
+++ internals/handlers/auth.handler.go
@@ -0,0 +1,94 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/metgag/koda-weekly10/internals/models"
+	"github.com/metgag/koda-weekly10/internals/repositories"
+	"github.com/metgag/koda-weekly10/pkg"
+)
+
+type AuthHandler struct {
+	ar *repositories.AuthRepository
+}
+
+func NewAuthHandler(ar *repositories.AuthRepository) *AuthHandler {
+	return &AuthHandler{ar: ar}
+}
+
+func NewAuthResponse(err string, success bool, result string) models.AuthResponse {
+	return models.AuthResponse{Error: err, Success: success, Result: result}
+}
+
+func (a *AuthHandler) AddUser(ctx *gin.Context) {
+	var body = models.Register{}
+
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		ctx.JSON(http.StatusInternalServerError, NewAuthResponse(
+			"server unable to bind request", false, "",
+		))
+		return
+	}
+
+	p := pkg.NewHashParams()
+	p.UseRecommended()
+	encodedHash, err := p.GenerateFromPassword(body.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, NewAuthResponse(
+			"server error while encoding password", false, "",
+		))
+		return
+	}
+
+	id, err := a.ar.AddNewUser(ctx.Request.Context(), body.Email, encodedHash)
+	if err != nil {
+		ctx.JSON(http.StatusConflict, NewAuthResponse(
+			"duplicate email addresses", false, "",
+		))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, NewAuthResponse(
+		"", true, fmt.Sprintf("register succesfully w/ ID: %d", id),
+	))
+}
+
+func (a *AuthHandler) Login(ctx *gin.Context) {
+	var body = models.Login{}
+
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		ctx.JSON(http.StatusInternalServerError, NewAuthResponse(
+			"server unable to bind request", false, "",
+		))
+		return
+	}
+
+	user, err := a.ar.GetUser(ctx.Request.Context(), body.Email)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, NewAuthResponse(
+			"server unable to get user", false, "",
+		))
+		return
+	}
+
+	hc := pkg.NewHashParams()
+	isMatch, err := hc.ComparePasswordAndHash(body.Password, user.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, NewAuthResponse(
+			"server unable to compare password", false, "",
+		))
+		return
+	}
+	if !isMatch {
+		ctx.JSON(http.StatusBadRequest, NewAuthResponse(
+			"invalid email or password", false, "",
+		))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, NewAuthResponse(
+		"", true, fmt.Sprintf("logged in as UID: %d", user.ID),
+	))
+}
