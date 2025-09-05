70ac6e78af223cf9d339ac595fca4e512823d97f
metgag <moreehigh1@gmail.com>
2025-09-06 06:36:01 +0700

    feat: add user router

---
+++ internals/routers/user.router.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/metgag/koda-weekly10/internals/handlers"
+	"github.com/metgag/koda-weekly10/internals/repositories"
+)
+
+func InitUserRouter(r *gin.Engine, dbpool *pgxpool.Pool) {
+	ur := repositories.NewUserRepository(dbpool)
+	uh := handlers.NewUserHandler(ur)
+
+	userGroup := r.Group("/userinf")
+	{
+		userGroup.GET("/:uid", uh.HandleUserinf)
+		userGroup.PATCH("/:uid", uh.HandleUpdateUserInf)
+	}
+}
