11c44e2dd682480736d159ad3c12475fe48862d1
metgag <moreehigh1@gmail.com>
2025-09-06 13:27:32 +0700

    feat: add cinema model model list: - schedule - available seat

---
+++ internals/models/cinema.model.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"time"
+
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type CinemaSchedule struct {
+	ID         uint16      `db:"id" json:"id"`
+	Title      string      `db:"title" json:"title"`
+	Date       pgtype.Date `db:"date" json:"date"`
+	Time       time.Time   `db:"time" json:"time"`
+	Location   string      `db:"location" json:"location"`
+	CinemaName string      `db:"cinema_name" json:"cinema_name"`
+}
+
+type ScheduleResponse struct {
+	Result  []CinemaSchedule
+	Success bool
+	Error   string
+}
+
+type AvailSeat struct {
+	ID  uint8  `db:"id" json:"id"`
+	Pos string `db:"pos" json:"pos"`
+}
+
+type AvailSeatsResponse struct {
+	Result  []AvailSeat
+	Success bool
+	Error   string
+}
