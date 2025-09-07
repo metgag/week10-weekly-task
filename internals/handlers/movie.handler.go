27ddfe4a122413cef90ee94cbfe33390acaef939
metgag <moreehigh1@gmail.com>
2025-09-07 22:29:08 +0700

    feat: update movie handler, add swag docs doc list: - get upcoming movies - get popular movies - get movie w/ filter by name and genre w/ pagination - get all movie (admin) - delete movie (admin) - edit movie (admin)

--- internals/handlers/movie.handler.go
+++ internals/handlers/movie.handler.go
@@ -22,6 +22,13 @@ func newMovieResponse(result []models.Movie, success bool, error string) models.
 	return models.MovieResponse{Result: result, Success: success, Error: error}
 }
 
+// HandleGetUpcomingMovies godoc
+// @Summary get upcoming movies handler func
+// @Tags movies
+// @Accept json
+// @Produce json
+// @Success 200 {object} models.MovieResponse
+// @Router /movies/upcoming [get]
 func (m *MovieHandler) GetUpcomingMovies(ctx *gin.Context) {
 	movies, err := m.mr.UpcomingMoviesDat(ctx.Request.Context())
 	if err != nil {
@@ -43,6 +50,13 @@ func (m *MovieHandler) GetUpcomingMovies(ctx *gin.Context) {
 	))
 }
 
+// HandleGetPopularMovies godoc
+// @Summary get popular movies handler func
+// @Tags movies
+// @Accept json
+// @Produce json
+// @Success 200 {object} models.MovieResponse
+// @Router /movies/popular [get]
 func (m *MovieHandler) GetPopularMovies(ctx *gin.Context) {
 	movies, err := m.mr.PopularMoviesDat(ctx.Request.Context())
 	if err != nil {
@@ -68,6 +82,14 @@ func NewMovieDetailResponse(res models.MovieDetail, success bool, err string) mo
 	return models.MovieDetailResponse{Result: res, Success: success, Error: err}
 }
 
+// HandleGetMovieDetail godoc
+// @Summary get movie detail handler func
+// @Tags movies
+// @Accept json
+// @Produce json
+// @Param        id   path      int  true  "movie ID"
+// @Success 200 {object} models.MovieDetailResponse
+// @Router /movies/:id [get]
 func (m *MovieHandler) GetMovieDetail(ctx *gin.Context) {
 	idParam, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
@@ -90,6 +112,13 @@ func (m *MovieHandler) GetMovieDetail(ctx *gin.Context) {
 	))
 }
 
+// HandleGetAllMovies godoc
+// @Summary get all movies (admin)
+// @Tags movies
+// @Accept json
+// @Produce json
+// @Success 200 {object} models.MovieResponse
+// @Router /admin/movies/ [get]
 func (m *MovieHandler) HandleGetAllMovie(ctx *gin.Context) {
 	movies, err := m.mr.GetAllMoviesDat(ctx.Request.Context())
 	if err != nil {
@@ -110,81 +139,107 @@ func (m *MovieHandler) HandleGetAllMovie(ctx *gin.Context) {
 	))
 }
 
+func newDeleteMovieResponse(success bool, res, err string) models.DeleteMovieResponse {
+	return models.DeleteMovieResponse{Success: success, Result: res, Error: err}
+}
+
+// HandleDeleteMovie godoc
+// @Summary delete a movie w/ ID
+// @Tags movies
+// @Accept json
+// @Produce json
+// @Param        id   path      int  true  "movie ID"
+// @Success 200 {object} models.DeleteMovieResponse
+// @Router /admin/movies/:id [delete]
 func (m *MovieHandler) HandleDeleteMovie(ctx *gin.Context) {
 	idParam, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"success": false,
-			"error":   "invalid movie id",
-		})
+		ctx.JSON(http.StatusBadRequest, newDeleteMovieResponse(
+			false, "", "invalid movie ID",
+		))
 		return
 	}
 
 	ctag, err := m.mr.DeleteMovie(ctx.Request.Context(), idParam)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"success": false,
-			"error":   "server unable to delete movie",
-		})
+		ctx.JSON(http.StatusInternalServerError, newDeleteMovieResponse(
+			false, "", "server unable to delete movie",
+		))
 		return
 	}
 
 	if ctag.RowsAffected() == 0 {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"success": false,
-			"error":   fmt.Sprintf("no movie w/ id: %d", idParam),
-		})
+		ctx.JSON(http.StatusBadRequest, newDeleteMovieResponse(
+			false, "", fmt.Sprintf("no movie w/ ID %d", idParam),
+		))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"success": true,
-		"result":  ctag.String(),
-	})
+	ctx.JSON(http.StatusOK, newDeleteMovieResponse(
+		true, fmt.Sprintf("movie w/ ID %d deleted succesfully", idParam), "",
+	))
 }
 
+func newUpdateMovieResponse(success bool, res, err string) models.DeleteMovieResponse {
+	return models.DeleteMovieResponse{Success: success, Result: res, Error: err}
+}
+
+// HandleUpdateMovie godoc
+// @Summary update a movie w/ ID
+// @Tags movies
+// @Accept json
+// @Produce json
+// @Param        id   path      int  true  "movie ID"
+// @Success 200 {object} models.UpdateMovieResponse
+// @Router /admin/movies/:id [patch]
 func (m *MovieHandler) HandleMovieUpdate(ctx *gin.Context) {
 	idParam, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"success": false,
-			"error":   "invalid movie id",
-		})
+		ctx.JSON(http.StatusBadRequest, newUpdateMovieResponse(
+			false, "", "invalid movie ID",
+		))
 		return
 	}
 
 	var newBody models.Movie
 	if err := ctx.ShouldBindJSON(&newBody); err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"success": false,
-			"error":   "server unable to bind data",
-		})
+		ctx.JSON(http.StatusInternalServerError, newUpdateMovieResponse(
+			false, "", "server unable to bind body",
+		))
 		return
 	}
 
 	ctag, err := m.mr.UpdateMovie(newBody, ctx.Request.Context(), idParam)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"success": false,
-			"error":   "server unable to update movie data",
-		})
+		ctx.JSON(http.StatusInternalServerError, newUpdateMovieResponse(
+			false, "", "server unable to update movie",
+		))
 		return
 	}
 
 	if ctag.RowsAffected() == 0 {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"success": false,
-			"error":   fmt.Sprintf("unable to make update, no movie w/ ID %d", idParam),
-		})
+		ctx.JSON(http.StatusInternalServerError, newUpdateMovieResponse(
+			false, "", fmt.Sprintf("no movie w/ ID %d", idParam),
+		))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"success": true,
-		"result":  ctag.String(),
-	})
+	ctx.JSON(http.StatusOK, newUpdateMovieResponse(
+		true, fmt.Sprintf("movie w/ ID %d updated succesfully", idParam), "",
+	))
 }
 
+// HandleGetMovieWithGenrePageSearch godoc
+// @Summary get movie with filter by name and genre with pagination
+// @Tags movies
+// @Accept json
+// @Produce json
+// @Param	example    query     string  false  "search title by q" example(gump)
+// @Param   int         query     int        false  "page number"	example(1)
+// @Param   default     query     int     false  "genre_id default"     default(0)
+// @Param   enumint     query     int        false  "genre_id enums"          Enums(27, 53, 12, 14, 28, 80, 18, 878, 35, 10749)
+// @Success 200 {object} models.MovieResponse
+// @Router /movies/ [get]
 func (m *MovieHandler) HandleMovieWithGenrePageSearch(ctx *gin.Context) {
 	q := ctx.Query("q")
 	genreId, err := strconv.Atoi(ctx.Query("genre_id"))
