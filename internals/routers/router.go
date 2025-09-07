3ba2b18e0e9b80d7e91aa2d26ad407e14261c4aa
metgag <moreehigh1@gmail.com>
2025-09-07 22:37:56 +0700

    feat: update router, import new packages docs, swaggerfiles, ginSwagger

--- internals/routers/router.go
+++ internals/routers/router.go
@@ -3,10 +3,17 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
+
+	docs "github.com/metgag/koda-weekly10/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitRouter(dbpool *pgxpool.Pool) *gin.Engine {
 	r := gin.Default()
+
+	docs.SwaggerInfo.BasePath = "/"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	InitAuthRouter(r, dbpool)
 	InitMovieRouter(r, dbpool)
