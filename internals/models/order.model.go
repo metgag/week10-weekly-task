257707bf04812ff89b913695a12a91155458dbfa
metgag <moreehigh1@gmail.com>
2025-09-07 22:35:58 +0700

    feat: update order model, add docs example example list: - payment method (cinema order) - title, cinema name (order history)

--- internals/models/order.model.go
+++ internals/models/order.model.go
@@ -9,18 +9,18 @@ import (
 type OrderHistory struct {
 	OrderID    uint16      `db:"id" json:"id"`
 	UserID     uint16      `db:"user_id" json:"user_id"`
-	Title      string      `db:"title" json:"title"`
+	Title      string      `db:"title" json:"title" example:"Pulp Fiction"`
 	Date       pgtype.Date `db:"date" json:"date"`
 	Time       time.Time   `db:"time" json:"time"`
-	CinemaName string      `db:"cinema_name" json:"cinema_name"`
+	CinemaName string      `db:"cinema_name" json:"cinema_name" example:"ebv"`
 	IsPaid     bool        `db:"is_paid" json:"is_paid"`
 }
 
 type CinemaOrder struct {
 	UID           uint16 `db:"user_id" json:"user_id"`
 	ScheduleID    uint16 `db:"schedule_id" json:"schedule_id"`
-	PaymentMethod string `db:"payment_method" json:"payment_method"`
-	Total         uint16 `db:"total" json:"total"`
+	PaymentMethod string `db:"payment_method" json:"payment_method" example:"PayPal"`
+	Total         uint16 `db:"total" json:"total" example:"30"`
 	IsPaid        bool   `db:"is_paid" json:"is_paid"`
 }
 
