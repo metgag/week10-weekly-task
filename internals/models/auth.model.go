becbff50153583422d5229452b5fcddb2602ae77
metgag <moreehigh1@gmail.com>
2025-09-07 22:25:59 +0700

    feat: update auth model, add register response and login response

--- internals/models/auth.model.go
+++ internals/models/auth.model.go
@@ -9,17 +9,23 @@ type User struct {
 
 type Register struct {
 	ID       uint16 `db:"id" json:"id"`
-	Email    string `db:"email" json:"email" binding:"required,email"`
+	Email    string `db:"email" json:"email" binding:"required,email" example:"foo_bar@mail.com"`
 	Password string `db:"password" json:"password" binding:"required,min=8,containsany=!@#$%^&*,containsany=ABCDEFGHIJKLMNOPQRSTUVWXYZ"`
 }
 
 type Login struct {
-	Email    string `db:"email" json:"email" binding:"required"`
+	Email    string `db:"email" json:"email" binding:"required" example:"foo_bar@mail.com"`
 	Password string `db:"password" json:"password" binding:"required"`
 }
 
-type AuthResponse struct {
-	Success bool
-	Result  string
-	Error   string
+type RegisterResponse struct {
+	Success bool   `example:"true"`
+	Result  string `example:"register succesfully w/ ID: 1"`
+	Error   string `example:"server unable to bind request"`
+}
+
+type LoginResponse struct {
+	Result  string `example:"logged in as UID 1"`
+	Success bool   `example:"true"`
+	Bearer  string
 }
