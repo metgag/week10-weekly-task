21b3e68155d5795aa400d4c1a2981a6e116a7eee
metgag <moreehigh1@gmail.com>
2025-09-07 15:04:43 +0700

    feat: add route to orders, with method post and get

---
+++ internals/routers/order.router.go
@@ -0,0 +1,21 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/metgag/koda-weekly10/internals/handlers"
+	"github.com/metgag/koda-weekly10/internals/middlewares"
+	"github.com/metgag/koda-weekly10/internals/repositories"
+)
+
+func InitOrderRouter(router *gin.Engine, dbpool *pgxpool.Pool) {
+	or := repositories.NewOrderRepository(dbpool)
+	oh := handlers.NewOrderHandler(or)
+
+	router.POST("/orders", oh.HandleCreateOrder)
+	router.GET("/orders",
+		middlewares.ValidateToken,
+		middlewares.Access("admin"),
+		oh.HandleGetOrderHistory,
+	)
+}
