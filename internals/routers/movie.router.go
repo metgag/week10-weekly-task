9a406c31eb65f604468010aae5291476f1f6ed16
metgag <moreehigh1@gmail.com>
2025-09-07 17:55:01 +0700

    feat: update movie repository update list: - route to "/movies", method GET (get all movies data) - route to "/:id", method DELETE (delete a movie based from id param) - route to ":/id", method PATCH (update movie data based from id param)

--- internals/routers/movie.router.go
+++ internals/routers/movie.router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/metgag/koda-weekly10/internals/handlers"
+	"github.com/metgag/koda-weekly10/internals/middlewares"
 	"github.com/metgag/koda-weekly10/internals/repositories"
 )
 
@@ -11,10 +12,25 @@ func InitMovieRouter(router *gin.Engine, dbpool *pgxpool.Pool) {
 	mr := repositories.NewMovieRepository(dbpool)
 	mh := handlers.NewMovieHandler(mr)
 
-	movieRouter := router.Group("/movie")
+	movieRouter := router.Group("/movies")
 	{
 		movieRouter.GET("/upcoming", mh.GetUpcomingMovies)
 		movieRouter.GET("/popular", mh.GetPopularMovies)
 		movieRouter.GET("/:id", mh.GetMovieDetail)
+		movieRouter.GET("",
+			middlewares.ValidateToken,
+			middlewares.Access("admin"),
+			mh.HandleGetAllMovie,
+		)
+		movieRouter.DELETE("/:id",
+			middlewares.ValidateToken,
+			middlewares.Access("admin"),
+			mh.HandleDeleteMovie,
+		)
+		movieRouter.PATCH("/:id",
+			middlewares.ValidateToken,
+			middlewares.Access("admin"),
+			mh.HandleMovieUpdate,
+		)
 	}
 }
