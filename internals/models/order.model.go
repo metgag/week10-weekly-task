43b2a686d434f478aab0f1c80e4ec9f4a7c6c450
metgag <moreehigh1@gmail.com>
2025-09-07 15:10:19 +0700

    feat: add cinema order and order response at order model

--- internals/models/order.model.go
+++ internals/models/order.model.go
@@ -8,9 +8,24 @@ import (
 
 type OrderHistory struct {
 	OrderID    uint16      `db:"id" json:"id"`
+	UserID     uint16      `db:"user_id" json:"user_id"`
 	Title      string      `db:"title" json:"title"`
 	Date       pgtype.Date `db:"date" json:"date"`
 	Time       time.Time   `db:"time" json:"time"`
 	CinemaName string      `db:"cinema_name" json:"cinema_name"`
 	IsPaid     bool        `db:"is_paid" json:"is_paid"`
+}
+
+type CinemaOrder struct {
+	UID           uint16 `db:"user_id" json:"user_id"`
+	ScheduleID    uint16 `db:"schedule_id" json:"schedule_id"`
+	PaymentMethod string `db:"payment_method" json:"payment_method"`
+	Total         uint16 `db:"total" json:"total"`
+	IsPaid        bool   `db:"is_paid" json:"is_paid"`
+}
+
+type OrderResponse struct {
+	Result  string
+	Success bool
+	Error   string
 }
