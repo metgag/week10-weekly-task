bf3bfc8dd5ee15c3d102aba1dc78bf1c2c72c18b
metgag <moreehigh1@gmail.com>
2025-09-07 22:25:01 +0700

    feat: update auth handler, add swag docs for register and login

--- internals/handlers/auth.handler.go
+++ internals/handlers/auth.handler.go
@@ -18,15 +18,23 @@ func NewAuthHandler(ar *repositories.AuthRepository) *AuthHandler {
 	return &AuthHandler{ar: ar}
 }
 
-func newAuthResponse(err string, success bool, result string) models.AuthResponse {
-	return models.AuthResponse{Error: err, Success: success, Result: result}
+func newRegisterResponse(err string, success bool, result string) models.RegisterResponse {
+	return models.RegisterResponse{Error: err, Success: success, Result: result}
 }
 
+// HandleRegister godoc
+// @Summary user register handler func
+// @Tags auth
+// @Accept json
+// @Produce json
+// @Param request body models.Register true "register body json content"
+// @Success 200 {object} models.RegisterResponse "successful register response"
+// @Router /auth/register [post]
 func (a *AuthHandler) AddUser(ctx *gin.Context) {
 	var body = models.Register{}
 
 	if err := ctx.ShouldBindJSON(&body); err != nil {
-		ctx.JSON(http.StatusInternalServerError, newAuthResponse(
+		ctx.JSON(http.StatusInternalServerError, newRegisterResponse(
 			"server unable to bind request", false, "",
 		))
 		return
@@ -36,7 +44,7 @@ func (a *AuthHandler) AddUser(ctx *gin.Context) {
 	p.UseRecommended()
 	encodedHash, err := p.GenerateFromPassword(body.Password)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, newAuthResponse(
+		ctx.JSON(http.StatusInternalServerError, newRegisterResponse(
 			"server error while encoding password", false, "",
 		))
 		return
@@ -44,22 +52,34 @@ func (a *AuthHandler) AddUser(ctx *gin.Context) {
 
 	id, err := a.ar.AddNewUser(ctx.Request.Context(), body.Email, encodedHash)
 	if err != nil {
-		ctx.JSON(http.StatusConflict, newAuthResponse(
+		ctx.JSON(http.StatusConflict, newRegisterResponse(
 			"duplicate email addresses", false, "",
 		))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, newAuthResponse(
+	ctx.JSON(http.StatusOK, newRegisterResponse(
 		"", true, fmt.Sprintf("register succesfully w/ ID: %d", id),
 	))
 }
 
+func newLoginResponse(res, bearer string, success bool) models.LoginResponse {
+	return models.LoginResponse{Result: res, Success: success, Bearer: bearer}
+}
+
+// HandleLogin godoc
+// @Summary user login handler func
+// @Tags auth
+// @Accept json
+// @Produce json
+// @Param request body models.Login true "login body json content"
+// @Success 200 {object} models.LoginResponse "successful login response"
+// @Router /auth/login [post]
 func (a *AuthHandler) Login(ctx *gin.Context) {
 	var body = models.Login{}
 
 	if err := ctx.ShouldBindJSON(&body); err != nil {
-		ctx.JSON(http.StatusInternalServerError, newAuthResponse(
+		ctx.JSON(http.StatusInternalServerError, newRegisterResponse(
 			"server unable to bind request", false, "",
 		))
 		return
@@ -67,7 +87,7 @@ func (a *AuthHandler) Login(ctx *gin.Context) {
 
 	user, err := a.ar.GetUser(ctx.Request.Context(), body.Email)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, newAuthResponse(
+		ctx.JSON(http.StatusInternalServerError, newRegisterResponse(
 			"server unable to get user", false, "",
 		))
 		return
@@ -76,13 +96,13 @@ func (a *AuthHandler) Login(ctx *gin.Context) {
 	hc := pkg.NewHashParams()
 	isMatch, err := hc.ComparePasswordAndHash(body.Password, user.Password)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, newAuthResponse(
+		ctx.JSON(http.StatusInternalServerError, newRegisterResponse(
 			"server unable to compare password", false, "",
 		))
 		return
 	}
 	if !isMatch {
-		ctx.JSON(http.StatusBadRequest, newAuthResponse(
+		ctx.JSON(http.StatusBadRequest, newRegisterResponse(
 			"invalid email or password", false, "",
 		))
 		return
@@ -91,19 +111,13 @@ func (a *AuthHandler) Login(ctx *gin.Context) {
 	claims := pkg.NewJWTClaims(user.ID, user.Role)
 	token, err := claims.GenAccessToken()
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, newAuthResponse(
+		ctx.JSON(http.StatusInternalServerError, newRegisterResponse(
 			"server unable to generate access token", false, "",
 		))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, struct {
-		ID      uint16 `json:"user_id"`
-		Success bool   `json:"success"`
-		Bearer  string `json:"bearer"`
-	}{
-		user.ID,
-		true,
-		token,
-	})
+	ctx.JSON(http.StatusOK, newLoginResponse(
+		fmt.Sprintf("logged in as UID %d", claims.UserID), token, true,
+	))
 }
