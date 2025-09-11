48e65ee5b212fc53e63a8df391d93dcb75c4c635
metgag <moreehigh1@gmail.com>
2025-09-11 15:14:01 +0700

    feat: update user handler, add edit user's password feature

--- internals/handlers/user.handler.go
+++ internals/handlers/user.handler.go
@@ -2,12 +2,12 @@ package handlers
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/metgag/koda-weekly10/internals/models"
 	"github.com/metgag/koda-weekly10/internals/repositories"
+	"github.com/metgag/koda-weekly10/internals/utils"
 	"github.com/metgag/koda-weekly10/pkg"
 )
 
@@ -38,6 +38,7 @@ func (u *UserHandler) HandleUserinf(ctx *gin.Context) {
 
 	userinf, err := u.ur.GetUserinf(ctx.Request.Context(), user.UserID)
 	if err != nil {
+		utils.PrintError(fmt.Sprintf("UNABLE TO REACH PROFILE WITH USER ID %d", user.UserID), 8, err)
 		ctx.JSON(http.StatusInternalServerError, newUserinfResponse(
 			models.UserInf{}, false, fmt.Sprintf("server unable to reach user w/ ID %d", user.UserID),
 		))
@@ -69,7 +70,7 @@ func (u *UserHandler) HandleUpdateUserInf(ctx *gin.Context) {
 
 	var newUserInf models.NewInf
 	if err := ctx.ShouldBindJSON(&newUserInf); err != nil {
-		log.Println(err.Error())
+		utils.PrintError("UNABLE TO BIND PROFILE UPDATE BODY", 12, err)
 		ctx.JSON(http.StatusInternalServerError, newUpdateResponse(
 			"", false, "server unable to bind input",
 		))
@@ -78,19 +79,21 @@ func (u *UserHandler) HandleUpdateUserInf(ctx *gin.Context) {
 
 	ctag, err := u.ur.UpdateUserinf(newUserInf, ctx, user.UserID)
 	if err != nil {
+		utils.PrintError("UNABLE TO MAKE UPDATE REQUEST", 12, err)
 		ctx.JSON(http.StatusInternalServerError, newUpdateResponse(
 			"", false, "server unable to update request",
 		))
 		return
 	}
 	if ctag.RowsAffected() == 0 {
-		if _, err := u.ur.InitUpdateUserinf(newUserInf, ctx, user.UserID); err == nil {
-			ctx.JSON(http.StatusCreated, newUpdateResponse(
-				fmt.Sprintf("user update created w/ ID %d", user.UserID), true, "",
-			))
-			return
-		}
+		// if _, err := u.ur.InitUpdateUserinf(newUserInf, ctx, user.UserID); err == nil {
+		// 	ctx.JSON(http.StatusCreated, newUpdateResponse(
+		// 		fmt.Sprintf("user update created w/ ID %d", user.UserID), true, "",
+		// 	))
+		// 	return
+		// }
 
+		utils.PrintError(fmt.Sprintf("UNABLE TO UPDATE PROFILE WITH USER ID %d", user.UserID), 8, err)
 		ctx.JSON(http.StatusBadRequest, newUpdateResponse(
 			"", false, fmt.Sprintf("there is no user w/ ID %d", user.UserID),
 		))
@@ -121,13 +124,71 @@ func (u *UserHandler) HandleUserOrderHistory(ctx *gin.Context) {
 
 	history, err := u.ur.GetUserOrderHistory(ctx.Request.Context(), user.UserID)
 	if err != nil {
+		utils.PrintError("UNABLE TO GET USER ORDER HISTORIES", 12, err)
 		ctx.JSON(http.StatusInternalServerError, newHistoryResponse(
 			models.UserOrder{}, false, "server unable to get user order history",
+		))
+		return
+	}
+	if len(history.OrderHistory) == 0 {
+		utils.PrintError("USER HAVE NO ORDER HISTORIES", 12, nil)
+		ctx.JSON(http.StatusNoContent, newHistoryResponse(
+			models.UserOrder{}, false, "user have no order yet",
 		))
 		return
 	}
 
 	ctx.JSON(http.StatusOK, newHistoryResponse(
 		history, true, "",
+	))
+}
+
+func newEditPasswordResponse(res, err string, success bool) models.EditPasswordResponse {
+	return models.EditPasswordResponse{Result: res, Error: err, Success: success}
+}
+
+func (u *UserHandler) HandlePasswordEdit(ctx *gin.Context) {
+	claims, _ := ctx.Get("claims")
+	user, _ := claims.(pkg.Claims)
+
+	var newPwdBody models.PasswordBody
+	if err := ctx.ShouldBindJSON(&newPwdBody); err != nil {
+		utils.PrintError("UNABLE BIND PASSWORD TO BODY", 12, err)
+		ctx.JSON(http.StatusBadRequest, newEditPasswordResponse(
+			"", "server unable to bind request", false,
+		))
+		return
+	}
+
+	p := pkg.NewHashParams()
+	p.UseRecommended()
+	newEncodedHash, err := p.GenerateFromPassword(newPwdBody.Password)
+	if err != nil {
+		utils.PrintError("UNABLE TO HASH NEW PASSWORD", 8, err)
+		ctx.JSON(http.StatusInternalServerError, newEditPasswordResponse(
+			"", "server error while encode new password", false,
+		))
+		return
+	}
+
+	ctag, err := u.ur.UpdateUserPassword(ctx.Request.Context(), newEncodedHash, user.UserID)
+	if err != nil {
+		utils.PrintError("SERVER UNABLE TO MAKE PASSWORD UPDATE", 8, err)
+		ctx.JSON(http.StatusInternalServerError, newEditPasswordResponse(
+			"", "server error while updating new password", false,
+		))
+		return
+	}
+
+	if ctag.RowsAffected() == 0 {
+		utils.PrintError(fmt.Sprintf("UNABLE TO UPDATE PASSWORD, NO USER ID %d", user.UserID), 8, err)
+		ctx.JSON(http.StatusBadRequest, newEditPasswordResponse(
+			"", fmt.Sprintf("there is no user w/ ID %d", user.UserID), false,
+		))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, newEditPasswordResponse(
+		fmt.Sprintf("succesfully update user's password w/ ID %d", user.UserID), "", true,
 	))
 }
