22458a2a78802070c8e543af18fc612bec36ab76
metgag <moreehigh1@gmail.com>
2025-09-22 05:03:47 +0700

    feat: update user model add form tag at newInf model, and json tag at historyResponse

--- internals/models/user.model.go
+++ internals/models/user.model.go
@@ -1,25 +1,38 @@
 package models
 
+import "mime/multipart"
+
 type UserInf struct {
 	UID         uint16  `db:"user_id" json:"user_id"`
 	FirstName   *string `db:"first_name" json:"first_name"`
 	LastName    *string `db:"last_name" json:"last_name"`
 	PhoneNumber *string `db:"phone_number" json:"phone_number" binding:"min=10.numeric" example:"08224422765"`
 	PointCount  float32 `db:"point_count" json:"point_count" example:"4.2"`
+	Avatar      *string `db:"avatar" json:"avatar"`
+	Role        string  `json:"role"`
 }
 
 type UserinfResponse struct {
-	Result  UserInf `json:"result"`
+	Result  UserInf `json:"result,omitempty"`
 	Success bool    `json:"success"`
-	Error   string  `json:"error"`
+	Error   string  `json:"error,omitempty"`
 }
 
 type NewInf struct {
-	FirstName   string  `db:"first_name" json:"first_name"`
-	LastName    string  `db:"last_name" json:"last_name"`
-	PhoneNumber string  `db:"phone_number" json:"phone_number" example:"08667728761"`
-	PointCount  float32 `db:"point_count" json:"point_count" example:"4.8"`
+	FirstName   *string               `db:"first_name" form:"first_name"`
+	LastName    *string               `db:"last_name" form:"last_name"`
+	PhoneNumber *string               `db:"phone_number" form:"phone_number" example:"08667728761"`
+	PointCount  *float32              `db:"point_count" form:"point_count" example:"4.8"`
+	Avatar      *multipart.FileHeader `db:"avatar" form:"avatar"`
 }
+
+// type NewInf struct {
+// 	FirstName   *string               `db:"first_name" json:"first_name"`
+// 	LastName    *string               `db:"last_name" json:"last_name"`
+// 	PhoneNumber *string               `db:"phone_number" json:"phone_number" example:"08667728761"`
+// 	PointCount  *float32              `db:"point_count" json:"point_count" example:"4.8"`
+// 	Avatar      *multipart.FileHeader `db:"avatar" form:"avatar"`
+// }
 
 type UpdateResponse struct {
 	Result  string `json:"result"`
@@ -33,7 +46,7 @@ type UserOrder struct {
 }
 
 type HistoryResponse struct {
-	Result  UserOrder
-	Success bool
-	Error   string
+	Result  UserOrder `json:"result"`
+	Success bool      `json:"success"`
+	Error   string    `json:"error"`
 }
