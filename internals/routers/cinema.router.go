077a6a320a382887babdddf122b6666d19eb3ba8
metgag <moreehigh1@gmail.com>
2025-09-22 05:10:55 +0700

    feat: update cinema router add middlewares to check if user's or admin at endpoints

--- internals/routers/cinema.router.go
+++ internals/routers/cinema.router.go
@@ -4,16 +4,24 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/metgag/koda-weekly10/internals/handlers"
+	"github.com/metgag/koda-weekly10/internals/middlewares"
 	"github.com/metgag/koda-weekly10/internals/repositories"
+	"github.com/redis/go-redis/v9"
 )
 
-func InitCinemaRouter(router *gin.Engine, dbpool *pgxpool.Pool) {
+func InitCinemaRouter(router *gin.Engine, dbpool *pgxpool.Pool, rdb *redis.Client) {
 	cr := repositories.NewCinemaRepository(dbpool)
 	ch := handlers.NewCinemaHandler(cr)
 
 	cinemaRouter := router.Group("/cinemas")
+	cinemaRouter.Use(
+		middlewares.ValidateToken(rdb),
+		middlewares.Access("user", "admin"),
+	)
+
 	{
 		cinemaRouter.GET("/schedules", ch.HandlerSchedule)
-		cinemaRouter.GET("/seats", ch.HandlerSeats)
+		cinemaRouter.GET("/:schedule_id/seats", ch.HandlerSeats)
+		cinemaRouter.GET("/:schedule_id/selected", ch.HandlerCinemaNameAndTime)
 	}
 }
