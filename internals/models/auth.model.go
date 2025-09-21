0d1d38638912c8221c8a65372671ac7535b8bd11
metgag <moreehigh1@gmail.com>
2025-09-22 04:58:03 +0700

    feat: update auth model add struct of passwordBody, editPasswordResponse, and LogoutResponse

--- internals/models/auth.model.go
+++ internals/models/auth.model.go
@@ -8,7 +8,7 @@ type User struct {
 }
 
 type Register struct {
-	ID       uint16 `db:"id" json:"id"`
+	// ID       uint16 `db:"id" json:"id"`
 	Email    string `db:"email" json:"email" binding:"required,email" example:"foo_bar@mail.com"`
 	Password string `db:"password" json:"password" binding:"required,min=8,containsany=!@#$%^&*,containsany=ABCDEFGHIJKLMNOPQRSTUVWXYZ"`
 }
@@ -19,13 +19,30 @@ type Login struct {
 }
 
 type RegisterResponse struct {
-	Success bool   `example:"true"`
-	Result  string `example:"register succesfully w/ ID: 1"`
-	Error   string `example:"server unable to bind request"`
+	Success bool   `json:"success"`
+	Result  string `json:"result,omitempty" example:"register succesfully w/ ID: 37"`
+	Error   string `json:"message,omitempty" example:"server unable to bind request"`
 }
 
 type LoginResponse struct {
-	Result  string `example:"logged in as UID 1"`
-	Success bool   `example:"true"`
-	Bearer  string
+	Result  string `json:"message" example:"logged in as UID 123"`
+	Success bool   `json:"success"`
+	Token   string `json:"token,omitempty" example:"eyJhbGciOiJIUzI1NiIsInR5cCI6..."`
+}
+
+type PasswordBody struct {
+	OldPassword string `json:"old_password" binding:"required"`
+	NewPassword string `db:"password" json:"new_password" binding:"required,min=8,containsany=!@#$%^&*,containsany=ABCDEFGHIJKLMNOPQRSTUVWXYZ"`
+}
+
+type EditPasswordResponse struct {
+	Result  string `json:"result,omitempty"`
+	Success bool   `json:"success"`
+	Error   string `json:"error,omitempty"`
+}
+
+type LogoutResponse struct {
+	Result  string `json:"result,omitempty" example:"logout succesfully"`
+	Success bool   `json:"success"`
+	Error   string `json:"error,omitempty" example:"server error while logout"`
 }
