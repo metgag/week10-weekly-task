c0a49181fcb90c43fa22767ffa308628af2a9185
metgag <moreehigh1@gmail.com>
2025-09-22 05:05:18 +0700

    feat: update cinemaRepository add query to get cinema name and time

--- internals/repositories/cinema.repository.go
+++ internals/repositories/cinema.repository.go
@@ -20,7 +20,7 @@ func (c *CinemaRepository) GetSchedule(ctx context.Context) ([]models.CinemaSche
 		SELECT 
 			s.id "schedule_id", m.title, s.date, t.time, l.location, c.name "cinema_name"
 		FROM 
-			cinema_schedule AS s
+			schedule AS s
 		JOIN
 			movies AS m ON s.movie_id = m.id
 		JOIN
@@ -49,33 +49,78 @@ func (c *CinemaRepository) GetSchedule(ctx context.Context) ([]models.CinemaSche
 	return schedules, nil
 }
 
-func (c *CinemaRepository) GetAvailSeats(ctx context.Context) ([]models.AvailSeat, error) {
+func (c *CinemaRepository) GetCinemaNameAndTime(ctx context.Context, scheduleId int) (models.CinemaAndTime, error) {
+	sql := `
+		SELECT ct.name, jt.time, ct.cinema_img
+		FROM schedule s
+		JOIN cinema_tayang ct ON ct.id = s.cinema_id 
+		JOIN jam_tayang jt ON jt.id = s.time_id
+		WHERE s.id = $1
+	`
+
+	var result models.CinemaAndTime
+	if err := c.dbpool.QueryRow(ctx, sql, scheduleId).Scan(
+		&result.CinemaName,
+		&result.Time,
+		&result.CinemaImg,
+	); err != nil {
+		return models.CinemaAndTime{}, err
+	}
+
+	return result, nil
+}
+
+func (c *CinemaRepository) GetAvailSeats(ctx context.Context, scheduleId int) ([]models.Seat, error) {
+	var scheduleExists bool
+	checkScheduleSql := `
+		SELECT EXISTS (
+			SELECT 1
+			FROM "orders"
+			WHERE schedule_id = $1
+		)
+	`
+	if err := c.dbpool.QueryRow(ctx, checkScheduleSql, scheduleId).Scan(&scheduleExists); err != nil {
+		return nil, err
+	}
+
+	// if schedule_id not found
+	if !scheduleExists {
+		return []models.Seat{}, nil
+	}
+
 	sql := `
 		SELECT 
 			s.id, s.pos
 		FROM 
 			seats AS s
-		LEFT JOIN 
-			books_seats AS b ON b.seat_id = s.id
 		WHERE 
-			b.seat_id IS NULL
+			s.id NOT IN (
+				SELECT bs.seat_id
+				FROM orders_seats bs
+				JOIN orders bt ON bs.order_id = bt.id
+				WHERE bt.schedule_id = $1
+			)
 		ORDER BY 
 			s.id ASC
+
 	`
-	rows, err := c.dbpool.Query(ctx, sql)
+	rows, err := c.dbpool.Query(ctx, sql, scheduleId)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var seats []models.AvailSeat
+	var seats []models.Seat
 	for rows.Next() {
-		var seat models.AvailSeat
-		if err := rows.Scan(&seat.ID, &seat.Pos); err != nil {
+		var seat models.Seat
+		if err := rows.Scan(
+			&seat.ID,
+			&seat.Pos,
+		); err != nil {
 			return nil, err
 		}
+
 		seats = append(seats, seat)
 	}
-
 	return seats, nil
 }
