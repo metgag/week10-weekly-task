e0b28aeeb503ba086f495942e71b971ec78d72d0
metgag <moreehigh1@gmail.com>
2025-09-22 05:01:34 +0700

    feat: update movie model add new models, genre, cast, movieSchedule, schedule, with responses

--- internals/models/movie.model.go
+++ internals/models/movie.model.go
@@ -1,49 +1,90 @@
 package models
 
 import (
+	"mime/multipart"
 	"time"
 )
 
 type Movie struct {
 	ID           uint32    `db:"id" json:"id" example:"680"`
-	Title        string    `db:"title" json:"title" example:"Pulp Fiction"`
-	BackdropPath string    `db:"backdrop_path" json:"backdrop_path"`
-	PosterPath   string    `db:"poster_path" json:"poster_path"`
-	ReleaseDate  time.Time `db:"release_date" json:"release_date" example:"1994-09-10"`
-	Runtime      uint16    `db:"runtime" json:"runtime" example:"154"`
-	Overview     string    `db:"overview" json:"overview"`
-	DirectorID   uint16    `db:"director_id" json:"director_id" example:"6"`
-	Popularity   float32   `db:"popularity" json:"popularity" example:"17.246"`
+	Title        string    `db:"title" json:"title" example:"Pulp Fiction" form:"title"`
+	BackdropPath *string   `db:"backdrop_path" json:"backdrop_path"`
+	PosterPath   *string   `db:"poster_path" json:"poster_path"`
+	ReleaseDate  time.Time `db:"release_date" json:"release_date" example:"1994-09-10" form:"release_date"`
+	Runtime      uint16    `db:"runtime" json:"runtime" example:"154" form:"runtime"`
+	Overview     string    `db:"overview" json:"overview" form:"overview"`
+	DirectorName string    `db:"director_name" json:"director_name" example:"Mick Jagger" form:"director_id"`
+	// Popularity   *float32  `db:"popularity" json:"popularity" example:"17.246" form:"popularity"`
+	Genres []Genre `db:"genres" json:"genres"`
+	Casts  []Cast  `db:"casts" json:"cast"`
+}
+
+type MovieBody struct {
+	Title       *string               `form:"title" db:"title"`
+	Overview    *string               `form:"overview" db:"overview"`
+	Runtime     *uint16               `form:"runtime" db:"runtime"`
+	ReleaseDate *string               `form:"release_date" db:"release_date"` // parse ke time.Time
+	Popularity  *float32              `form:"popularity" db:"popularity"`
+	DirectorID  *uint32               `form:"director_id" db:"director_id"`
+	NewBackdrop *multipart.FileHeader `form:"backdrop_path"`
+	NewPoster   *multipart.FileHeader `form:"poster_path"`
+}
+
+// type MovieBody struct {
+// 	Title       string                `form:"title"`
+// 	Overview    string                `form:"overview"`
+// 	Runtime     uint16                `form:"runtime"`
+// 	ReleaseDate string                `form:"release_date"` // optionally parse to time.Time
+// 	Popularity  float32               `form:"popularity"`
+// 	DirectorID  uint32                `form:"director_id"` // gunakan ID, bukan nama
+// 	NewBackdrop *multipart.FileHeader `form:"backdrop_path"`
+// 	NewPoster   *multipart.FileHeader `form:"poster_path"`
+// }
+
+type MoviesResponse struct {
+	Result  []MovieFilter `json:"result"`
+	Success bool          `json:"success"`
+	Error   string        `json:"error"`
 }
 
 type MovieResponse struct {
-	Result  []Movie `json:"result"`
-	Success bool    `json:"success"`
-	Error   string  `json:"error"`
+	Result  Movie  `json:"result"`
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
 }
 
-type MovieDetail struct {
-	ID           uint32    `db:"id" json:"id" example:"680"`
-	Title        string    `db:"title" json:"title" example:"Pulp Fiction"`
-	BackdropPath string    `db:"backdrop_path" json:"backdrop_path"`
-	PosterPath   string    `db:"poster_path" json:"poster_path"`
-	ReleaseDate  time.Time `db:"release_date" json:"release_date" example:"1994-09-10"`
-	Runtime      uint16    `db:"runtime" json:"runtime" example:"154"`
-	Overview     string    `db:"overview" json:"overview"`
-	DirectorName string    `db:"director_name" json:"director_name" example:"Quentin Tarantino"`
-	Genres       []string  `db:"genres_name" json:"genres_name"`
+// type MovieDetail struct {
+// 	ID           uint32    `db:"id" json:"id" example:"680"`
+// 	Title        string    `db:"title" json:"title" example:"Pulp Fiction"`
+// 	BackdropPath string    `db:"backdrop_path" json:"backdrop_path"`
+// 	PosterPath   string    `db:"poster_path" json:"poster_path"`
+// 	ReleaseDate  time.Time `db:"release_date" json:"release_date" example:"1994-09-10"`
+// 	Runtime      uint16    `db:"runtime" json:"runtime" example:"154"`
+// 	Overview     string    `db:"overview" json:"overview"`
+// 	DirectorName string    `db:"director_name" json:"director_name" example:"Quentin Tarantino"`
+// 	Genres       *[]string `db:"genres_name" json:"genres_name"`
+// }
+
+// type MovieDetailResponse struct {
+// 	Result  MovieDetail `json:"result"`
+// 	Success bool        `json:"success"`
+// 	Error   string      `json:"error"`
+// }
+
+type MovieFilter struct {
+	ID          uint32    `db:"id" json:"id"`
+	Title       string    `db:"title" json:"title"`
+	PosterPath  *string   `db:"poster_path" json:"poster_path"`
+	ReleaseDate time.Time `db:"release_date" json:"release_date"`
+	Popularity  float32   `db:"popularity" json:"popularity"`
+	Runtime     uint16    `db:"runtime" json:"runtime"`
+	Genres      []Genre   `db:"genres" json:"genres"`
 }
 
-type MovieDetailResponse struct {
-	Result  MovieDetail `json:"result"`
-	Success bool        `json:"success"`
-	Error   string      `json:"error"`
-}
-
-type MovieGenre struct {
-	ID      uint32 `db:"movie_id" json:"movie_id"`
-	Title   string `db:"title" json:"title"`
-	GenreID uint16 `db:"genre_id" json:"genre_id"`
+type MovieFilterResponse struct {
+	Result  []MovieFilter `json:"result"`
+	Success bool          `json:"success"`
+	Error   string        `json:"error"`
 }
 
 type DeleteMovieResponse struct {
@@ -56,4 +97,74 @@ type UpdateMovieResponse struct {
 	Success bool
 	Result  string `example:"movie w/ ID 1 updated succesfully"`
 	Error   string
+}
+
+type Genre struct {
+	ID   uint32 `json:"id"`
+	Name string `json:"name"`
+}
+
+type Cast struct {
+	ID   uint16 `json:"id"`
+	Name string `json:"name"`
+}
+
+type MovieSchedule struct {
+	MovieID  uint32     `json:"movie_id"`
+	Schedule []Schedule `json:"schedule"`
+}
+
+type Schedule struct {
+	ScheduleID uint16    `json:"schedule_id"`
+	Date       time.Time `json:"date"`
+	Time       string    `json:"time"`
+	Location   string    `json:"location"`
+	Cinema     string    `json:"cinema"`
+	CinemaImg  string    `json:"cinema_img"`
+}
+
+type MovieSchedulesResponse struct {
+	Result  MovieSchedule `json:"result"`
+	Success bool          `json:"success"`
+	Error   string        `json:"error"`
+}
+
+type CreateMovieBody struct {
+	DirectorName string                `form:"director_name" binding:"required"`
+	Title        string                `form:"title" binding:"required"`
+	BackdropPath *multipart.FileHeader `form:"backdrop_path"`
+	PosterPath   *multipart.FileHeader `form:"poster_path"`
+	ReleaseDate  string                `form:"release_date" binding:"required"`
+	Runtime      uint16                `form:"runtime"`
+	Overview     string                `form:"overview"`
+	// Popularity   float32               `form:"popularity"`
+	Genres       string `form:"genres"`
+	Casts        string `form:"casts"`
+	LocationID   []int  `form:"location"`
+	ScheduleDate string `form:"schedule_date"`
+	TimeID       []int  `form:"schedule_time"`
+}
+
+type CreateMovieResponse struct {
+	Result  string `json:"result,omitempty"`
+	Success bool   `json:"success"`
+	Error   string `json:"error,omitempty"`
+}
+
+type Director struct {
+	ID   uint16 `db:"id"`
+	Name string `db:"name"`
+}
+
+type MovieScheduleFilter struct {
+	ScheduleID uint16 `json:"schedule_id"`
+	CinemaID   uint16 `json:"cinema_id"`
+	CinemaName string `json:"cinema_name"`
+	CinemaImg  string `json:"cinema_img"`
+}
+
+type MovieScheduleFilterResponse struct {
+	Result  []MovieScheduleFilter `json:"result"`
+	Success bool                  `json:"success"`
+	Error   string                `json:"error"`
 }
