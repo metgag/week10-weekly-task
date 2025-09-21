6354b1b79ce12fe63e588cb6296d5523c8c1066a
metgag <moreehigh1@gmail.com>
2025-09-22 05:12:45 +0700

    feat: update movie router add endpoint to get movie schedule

--- internals/routers/movie.router.go
+++ internals/routers/movie.router.go
@@ -5,17 +5,21 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/metgag/koda-weekly10/internals/handlers"
 	"github.com/metgag/koda-weekly10/internals/repositories"
+	"github.com/redis/go-redis/v9"
 )
 
-func InitMovieRouter(router *gin.Engine, dbpool *pgxpool.Pool) {
-	mr := repositories.NewMovieRepository(dbpool)
+func InitMovieRouter(router *gin.Engine, dbpool *pgxpool.Pool, rdb *redis.Client) {
+	mr := repositories.NewMovieRepository(dbpool, rdb)
 	mh := handlers.NewMovieHandler(mr)
 
 	movieRouter := router.Group("/movies")
+
 	{
 		movieRouter.GET("/upcoming", mh.GetUpcomingMovies)
 		movieRouter.GET("/popular", mh.GetPopularMovies)
-		movieRouter.GET("/", mh.HandleMovieWithGenrePageSearch)
+		movieRouter.GET("", mh.HandleMovieWithGenrePageSearch)
 		movieRouter.GET("/:id", mh.GetMovieDetail)
+		movieRouter.GET("/:id/schedules", mh.HandleGetMovieSchedule)
+		movieRouter.GET("/:id/schedule", mh.HandleGetMovieScheduleFilter)
 	}
 }
