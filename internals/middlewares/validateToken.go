9cc46b866af39d5e1a171cd334a8e8329d94f812
metgag <moreehigh1@gmail.com>
2025-09-22 04:59:29 +0700

    feat: update validate token middleware add check condition, is user's token blacklisted

--- internals/middlewares/validateToken.go
+++ internals/middlewares/validateToken.go
@@ -1,57 +1,64 @@
 package middlewares
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/metgag/koda-weekly10/internals/utils"
 	"github.com/metgag/koda-weekly10/pkg"
+	"github.com/redis/go-redis/v9"
 )
 
-// type accessResponse struct {
-// 	success bool
-// 	error   string
-// }
+type validateResponse struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
+}
 
-// func newAccessResponse(success bool, err string) accessResponse {
-// 	return accessResponse{success: success, error: err}
-// }
+func newAccessResponse(success bool, err string) validateResponse {
+	return validateResponse{Success: success, Error: err}
+}
 
-func ValidateToken(ctx *gin.Context) {
-	bearerToken := ctx.GetHeader("Authorization")
-	if bearerToken == "" {
-		ctx.AbortWithStatusJSON(http.StatusUnauthorized,
-			gin.H{
-				"success": false,
-				"error":   "need token to access this feature",
-			})
-		return
+func ValidateToken(rdb *redis.Client) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		bearerToken := ctx.GetHeader("Authorization")
+		if bearerToken == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized,
+				newAccessResponse(false, "need token to access this feature"))
+			return
+		}
+
+		splitToken := strings.Split(bearerToken, " ")
+		if len(splitToken) != 2 || !strings.EqualFold(splitToken[0], "Bearer") {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized,
+				newAccessResponse(false, "token unrecognized"))
+			return
+		}
+		token := splitToken[1]
+
+		redisKey := fmt.Sprintf("archie:blacklist_%s", token)
+		result, err := rdb.Get(ctx, redisKey).Result()
+		if err != nil && err != redis.Nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized,
+				newAccessResponse(false, "authorization service unavailable"))
+			return
+		}
+		if result == "1" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized,
+				newAccessResponse(false, "access token revoked"))
+			return
+		}
+
+		var claims pkg.Claims
+		if err := claims.ValidateToken(token); err != nil {
+			utils.PrintError("ERROR VALIDATE TOKEN", 16, err)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized,
+				newAccessResponse(false, "token expired, sign in again to get a new valid token"))
+			return
+		}
+
+		ctx.Set("claims", claims)
+		ctx.Next()
 	}
-
-	splitToken := strings.Split(bearerToken, " ")
-	if len(splitToken) != 2 {
-		ctx.AbortWithStatusJSON(http.StatusUnauthorized,
-			gin.H{
-				"success": false,
-				"error":   "token unrecognized",
-			})
-		return
-	}
-
-	token := splitToken[1]
-	var claims pkg.Claims
-	if err := claims.ValidateToken(token); err != nil {
-		log.Printf("%s VALIDATE TOKEN ERROR %s", strings.Repeat("=", 8), strings.Repeat("=", 8))
-		log.Println(err.Error())
-		ctx.AbortWithStatusJSON(http.StatusUnauthorized,
-			gin.H{
-				"success": false,
-				"error":   "token expired, sign in again to get a new valid token",
-			})
-		return
-	}
-
-	ctx.Set("claims", claims)
-	ctx.Next()
 }
