e030069625bf096f61aa3568cfbb4889dcd00985
metgag <moreehigh1@gmail.com>
2025-09-22 05:15:53 +0700

    feat: add new middleware cors whitelisting origin to get access to API

---
+++ internals/middlewares/cors.go
@@ -0,0 +1,34 @@
+package middlewares
+
+import (
+	"net/http"
+	"slices"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware(ctx *gin.Context) {
+	// allowed pages
+	whitelist := []string{
+		"http://localhost:5173",
+	}
+
+	// curr origin
+	origin := ctx.GetHeader("Origin")
+	if slices.Contains(whitelist, origin) {
+		ctx.Header("Access-Control-Allow-Origin", origin)
+	}
+	// else {
+	// 	utils.PrintError("ORIGIN NOT WHITELISTED", 20, nil)
+	// }
+
+	ctx.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+	ctx.Header("Access-Control-Allow-Headers", "Authorization, Content-Type")
+	// handler jika bertemu preflight
+	if ctx.Request.Method == http.MethodOptions {
+		ctx.AbortWithStatus(http.StatusNoContent)
+		return
+	}
+
+	ctx.Next()
+}
