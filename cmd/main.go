78303e1ce1d8889b636efc523a3466268ec17a65
metgag <moreehigh1@gmail.com>
2025-09-22 04:45:09 +0700

    feat: add swaggo docs main function title, version, description

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
-	"github.com/metgag/koda-weekly10/internals/config"
+	config "github.com/metgag/koda-weekly10/internals/configs"
 	"github.com/metgag/koda-weekly10/internals/routers"
 )
 
+//	@title			LOKET TIKET
+//	@version		1.0
+//	@description	RESTful API of tixkitz ticket systeme
+
+//	@host		localhost:6011
+//	@basepath	/
+
+// @securityDefinitions.apikey	BearerAuth
+// @in							header
+// @name						Authorization
 func main() {
 	// load dotenv
 	if err := godotenv.Load(); err != nil {
@@ -26,9 +38,18 @@ func main() {
 	if err := config.PingDB(dbpool); err != nil {
 		fmt.Fprintf(os.Stderr, "unable to connect to database: %v\n", err)
 	} else {
-		log.Printf("\\conninfo: connected to database \"%s\" as user \"%s\"", os.Getenv("DB_NAME"), os.Getenv("DB_USER"))
+		log.Printf("\\conninfo: connected to database \"%s\" as user \"%s\"", os.Getenv("DB_NAME_M"), os.Getenv("DB_USER"))
 	}
 
-	router := routers.InitRouter(dbpool)
+	// init redis
+	rdb := config.InitRedis()
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		log.Printf("unable to connect to redis server")
+	} else {
+		log.Printf("\nREDIS> PING\n%sPONG", strings.Repeat(" ", 7))
+	}
+	defer rdb.Close()
+
+	router := routers.InitRouter(dbpool, rdb)
 	router.Run("localhost:6011")
 }
