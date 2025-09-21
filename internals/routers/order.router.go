f7e9a29da99f5828afcb4cf844abf3a3a5aa5a4c
metgag <moreehigh1@gmail.com>
2025-09-22 05:15:12 +0700

    feat: update order router add endpoint for user's create order

--- internals/routers/order.router.go
+++ internals/routers/order.router.go
@@ -6,16 +6,16 @@ import (
 	"github.com/metgag/koda-weekly10/internals/handlers"
 	"github.com/metgag/koda-weekly10/internals/middlewares"
 	"github.com/metgag/koda-weekly10/internals/repositories"
+	"github.com/redis/go-redis/v9"
 )
 
-func InitOrderRouter(router *gin.Engine, dbpool *pgxpool.Pool) {
+func InitOrderRouter(router *gin.Engine, dbpool *pgxpool.Pool, rdb *redis.Client) {
 	or := repositories.NewOrderRepository(dbpool)
 	oh := handlers.NewOrderHandler(or)
 
-	router.POST("/orders", middlewares.ValidateToken, oh.HandleCreateOrder)
-	router.GET("/admin/orders",
-		middlewares.ValidateToken,
-		middlewares.Access("admin"),
-		oh.HandleGetOrderHistory,
+	router.POST("/orders",
+		middlewares.ValidateToken(rdb),
+		middlewares.Access("user"),
+		oh.HandleCreateOrder,
 	)
 }
