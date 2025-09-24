09b5cac97cda6e74deb9dbbcde2fa5cb21c55be9
metgag <moreehigh1@gmail.com>
2025-09-24 08:43:16 +0700

    refactor: change pg db config

--- internals/configs/db.go
+++ internals/configs/db.go
@@ -2,13 +2,23 @@ package configs
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func InitDB() (*pgxpool.Pool, error) {
-	return pgxpool.New(context.Background(), os.Getenv("DB_URL_M"))
+	user := os.Getenv("DB_USER")
+	pwd := os.Getenv("DB_PWD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	db := os.Getenv("DB_NAME_M")
+
+	connstring := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s", user, pwd, host, port, db,
+	)
+	return pgxpool.New(context.Background(), connstring)
 }
 
 func PingDB(p *pgxpool.Pool) error {
